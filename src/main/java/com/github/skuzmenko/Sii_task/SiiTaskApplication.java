5b09c39a25fae9ab75a78cb4178572cd5834b68b
SophieKuzmenko <sophie.s.kuzmenko@gmail.com>
2025-05-07 16:16:32 +0300

    first commit

---
+++ src/main/java/com/github/skuzmenko/Sii_task/SiiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.skuzmenko.Sii_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/skuzmenko/Sii_task/SiiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.skuzmenko.Sii_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
